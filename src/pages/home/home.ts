b342c00a0c6af1c3d678dde296a0978ab159464e
Madson Chavante <ml.chavante@gmail.com>
2018-07-22 23:17:56 -0300

    splash

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,18 +1,54 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { SolicitacaoPage } from '../solicitacao/solicitacao';
-import { ExtratoPage } from '../extrato/extrato';
+import { InAppBrowser, InAppBrowserOptions } from '@ionic-native/in-app-browser';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  options: InAppBrowserOptions = {
+    location: 'yes',//Or 'no'
+    hidden: 'no', //Or  'yes'
+    clearcache: 'yes',
+    clearsessioncache: 'yes',
+    zoom: 'no',//Android only ,shows browser zoom controls
+    hardwareback: 'yes',
+    mediaPlaybackRequiresUserAction: 'no',
+    shouldPauseOnSuspend: 'no', //Android only
+    closebuttoncaption: 'Close', //iOS only
+    disallowoverscroll: 'no', //iOS only
+    toolbar: 'yes', //iOS only
+    enableViewportScale: 'no', //iOS only
+    allowInlineMediaPlayback: 'no',//iOS only
+    presentationstyle: 'pagesheet',//iOS only
+    fullscreen: 'yes',//Windows only
+  };
 
-  constructor(public navCtrl: NavController) {
+  painelColor = 'primary';
+  tabBarElement: any;
+  splash = true;
+
+  constructor(public navCtrl: NavController, private theInAppBrowser: InAppBrowser) {
+    this.tabBarElement = document.querySelector('.tabbar');
   }
 
-  Root1 = SolicitacaoPage;
-  Root2 = ExtratoPage;
+  ionViewDidLoad() {
+    this.tabBarElement.style.display = 'none';
+    setTimeout(() => {
+      this.splash = false;
+      this.tabBarElement.style.display = 'flex';
+    }, 4000);
+  }
+  openWithInAppBrowser(url: string) {
+    let target = "_blank";
+    this.theInAppBrowser.create(url, target, this.options);
+  }
+  goSolicitacao() {
+    this.navCtrl.parent.select(1);
+  }
+  goExtrato() {
+    this.navCtrl.parent.select(2);
+  }
 
 }
